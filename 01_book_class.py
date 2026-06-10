8ee2ecd4545b7ecc95ee5ae1b517a45af8584cee
yadavsarvesh412005-eng <yadav.sarvesh412005@gmail.com>
2026-06-10 11:04:13 +0530

    Added Q1 output

---
+++ 01_book_class.py
@@ -0,0 +1,15 @@
+#  Book class and search by author
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+def search_author(books, author):
+    return [b.title for b in books if b.author == author]
+
+b1 = Book("Python", "John")
+b2 = Book("Java", "Mike")
+b3 = Book("C++", "John")
+
+print(search_author([b1, b2, b3], "John"))
